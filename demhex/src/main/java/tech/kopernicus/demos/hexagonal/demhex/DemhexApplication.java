682d82a5beea5c1fcec787034624043357465c80
rorolopezg <rodrigo.lopez.gatica@gmail.com>
2022-10-17 07:09:47 -0700

    Initial project.

---
+++ demhex/src/main/java/tech/kopernicus/demos/hexagonal/demhex/DemhexApplication.java
@@ -0,0 +1,13 @@
+package tech.kopernicus.demos.hexagonal.demhex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemhexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemhexApplication.class, args);
+	}
+
+}
---
+++ demhex/src/test/java/tech/kopernicus/demos/hexagonal/demhex/DemhexApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kopernicus.demos.hexagonal.demhex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemhexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
