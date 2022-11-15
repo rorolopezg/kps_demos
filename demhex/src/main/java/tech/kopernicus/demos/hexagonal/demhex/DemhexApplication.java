b0627af6577d70da97998e78e8cfebb0a4525d6a
rorolopezg <rodrigo.lopez.gatica@gmail.com>
2022-11-15 16:51:45 -0300

    últimos cambios

--- demhex/src/main/java/tech/kopernicus/demos/hexagonal/demhex/DemhexApplication.java
+++ demhex/src/main/java/tech/kopernicus/demos/hexagonal/demhex/DemhexApplication.java
@@ -1,11 +1,19 @@
 package tech.kopernicus.demos.hexagonal.demhex;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemhexApplication {
+public class DemhexApplication implements CommandLineRunner {
+	@Value("{}")
 	public static void main(String[] args) {
 		SpringApplication.run(DemhexApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
